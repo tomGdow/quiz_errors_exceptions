a44c974bd31f9c40401b457153c7b8d35000b99f
tomgdow <thomasgdowling@gmail.com>
2021-09-05 21:22:18 +0100

    initial commit of code

---
+++ quiz_todo.py
@@ -0,0 +1,30 @@
+
+    """ problems
+
+       "mystring".getTime()   AttributeError
+       
+       print "hello"          SyntaxError
+
+       File Not found error
+
+       with open('mynonfile.txt') as f:
+...   f.read()
+
+      1 + 2 + "three"     // Type error
+
+      3/ "two"                         // ValueError
+
+      import math
+      math.sqrt(-1)
+
+      ab = int('2') // ValueError
+
+
+      ValueError is a subclass of Exception
+      ZeroDivisionError is a subclass of ArithmeticError which is a subclass of Exception
+
+      mydict.pop()  // No argument!! // TypeError
+
+      'Simon's millions'    // Syntax error
+      
+    """
