6557ead74f8542e89e0e76a89507677613844e5b
tomgdow <thomasgdowling@gmail.com>
2021-09-07 11:03:22 +0100

    create problem template

---
+++ problem_template.py
@@ -0,0 +1,14 @@
+def problem_template():         
+    """ DOCSTRING HERE 
+
+    """
+
+    problem = """{: >4s}
+    
+
+    """.format("")
+
+    answer = ("ZZ", d.errors['ZZ'])
+    answer_explanation = "GIVE EXPLANATION"
+    error_hierarchy = d.hierarchy['ZZ']
+    return(problem, answer, answer_explanation,error_hierarchy)
