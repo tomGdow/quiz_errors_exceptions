0fa18a9bc7a6a4796b9c19d254e98e6a94b27873
tomgdow <thomasgdowling@gmail.com>
2021-09-10 10:22:14 +0100

    add

--- quiz.py
+++ quiz.py
@@ -32,13 +32,17 @@ if __name__ == '__main__':
             p.problem_twenty_four,
             p.problem_twenty_five,
             p.problem_twenty_six,
+            p.problem_twenty_seven,
+            p.problem_twenty_eight,
+            p.problem_twenty_nine,
+            p.problem_thirty,        
             ]
     random_problem_list = random.sample(problem_list, k=len(problem_list)) # k = probem_number
     question_counter = f.counter(len(problem_list))
     f.display_problem(random_problem_list[0],question_counter, len(problem_list))
     i = 1
     quit = False
-    while not quit and i < 26:  # i < problem_number
+    while not quit and i < len(problem_list):  # i < problem_number
         choice =f.next_question(random_problem_list[i], f.continue_choice, question_counter, len(problem_list))
         i += 1
         quit = not choice
