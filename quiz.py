ff4146f69f1b66c28b48f0107a8b554281f30ac3
tomgdow <thomasgdowling@gmail.com>
2021-09-27 12:22:38 +0100

    correct .today/.days and other typos in problems_wuiz_errors.py

--- quiz.py
+++ quiz.py
@@ -55,6 +55,8 @@ if __name__ == '__main__':
             p.problem_forty_seven,        
             p.problem_forty_eight,        
             p.problem_forty_nine,        
+            p.problem_fifty,        
+            p.problem_fifty_one,        
             ]
     random_problem_list = random.sample(problem_list, k=len(problem_list)) # k = probem_number
     question_counter = f.counter(len(problem_list))
