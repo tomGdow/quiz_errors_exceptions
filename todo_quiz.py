fde523321683178f8bfa34187a5526a31bfe3cd2
tomgdow <thomasgdowling@gmail.com>
2021-10-10 22:43:34 +0100

    update todo list with UnboundLocalError example

--- todo_quiz.py
+++ todo_quiz.py
@@ -1,6 +1,15 @@
 
         """ problems
 
+        >>> def myfn(a):
+...     x +=a
+...     return x
+...
+>>> myfn(20)
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+  File "<stdin>", line 2, in myfn
+UnboundLocalError: local variable 'x' referenced before assignment
 
 
 
