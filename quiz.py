bf6aded73976f81f901e6c8b9542cc71b11903da
tomgdow <thomasgdowling@gmail.com>
2021-10-23 12:35:40 +0100

    add problems hundred_six, hundred_seven, hundred_eight

--- quiz.py
+++ quiz.py
@@ -112,6 +112,8 @@ if __name__ == '__main__':
             p.problem_hundred_four,        
             p.problem_hundred_five,        
             p.problem_hundred_six,        
+            p.problem_hundred_seven,        
+            p.problem_hundred_eight,        
             ]
     random_problem_list = random.sample(problem_list, k=len(problem_list)) # k = problem_number
     question_counter = f.counter(len(problem_list))
